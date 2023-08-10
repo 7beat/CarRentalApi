7fdb7d38cb7253c7dcd4ffae40f4e43f8306a974
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:51:46 +0200

    Added Identity configuration

--- CarRental.Infrastructure/InfrastructureServicesRegistration.cs
+++ CarRental.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,9 @@
-﻿using CarRental.Infrastructure.Persistence.Data;
+﻿using CarRental.Application.Contracts.Identity;
+using CarRental.Infrastructure.Identity.Models;
+using CarRental.Infrastructure.Persistence.Data;
+using CarRental.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +15,9 @@ public static class InfrastructureServicesRegistration
     public static void RegisterInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.ConfigureDbContext(configuration);
+        services.AddTransient<IAuthService, AuthService>();
+        services.AddTransient<IAuthorizationHandler, RoleAuthorizationHandler>();
+        services.ConfigureIdentity();
     }
 
     private static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
@@ -22,5 +30,18 @@ public static class InfrastructureServicesRegistration
                 sqloptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
             });
         });
+    }
+
+    private static void ConfigureIdentity(this IServiceCollection services)
+    {
+        var builder = services.AddIdentity<ApplicationUser, IdentityRole<int>>(o =>
+        {
+            o.Password.RequireDigit = false;
+            o.Password.RequireUppercase = false;
+
+            o.User.RequireUniqueEmail = true;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
     }
 }
