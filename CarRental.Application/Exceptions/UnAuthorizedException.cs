702849912a39b390a3df0da8cf952c89a3bf7586
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 22:28:53 +0200

    Added Exceptions

---
+++ CarRental.Application/Exceptions/UnAuthorizedException.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRental.Application.Exceptions;
+public class UnAuthorizedException : Exception
+{
+    public UnAuthorizedException(string message) : base(message)
+    {
+    }
+
+    public UnAuthorizedException(string message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
