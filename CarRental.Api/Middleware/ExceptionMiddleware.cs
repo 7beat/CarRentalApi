58d081b51bef9f9d72341a01907f1558f768a4f1
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 22:28:41 +0200

    Added ExceptionMiddleware

---
+++ CarRental.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using CarRental.Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CarRental.Api.Middleware;
+
+public class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+    {
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        ProblemDetails problem = new();
+
+        switch (ex)
+        {
+            case BadRequestException badRequest:
+                statusCode = HttpStatusCode.BadRequest;
+                problem = new()
+                {
+                    Title = badRequest.Message,
+                    Status = (int)statusCode,
+                    Detail = badRequest.InnerException?.Message,
+                    Type = nameof(BadRequestException)
+                };
+                break;
+            case UnAuthorizedException unAuthorizedException:
+                statusCode = HttpStatusCode.Unauthorized;
+                problem = new()
+                {
+                    Title = unAuthorizedException.Message,
+                    Status = (int)statusCode,
+                    Detail = unAuthorizedException.InnerException?.Message,
+                    Type = nameof(UnAuthorizedException)
+                };
+                break;
+            default:
+                problem = new()
+                {
+                    Title = "An unexpected error occurred.",
+                    Status = (int)statusCode,
+                    Detail = ex.Message,
+                    Type = nameof(Exception)
+                };
+                break;
+        }
+
+        httpContext.Response.StatusCode = (int)statusCode;
+        await httpContext.Response.WriteAsJsonAsync(problem);
+    }
+}
--- CarRental.Api/Program.cs
+++ CarRental.Api/Program.cs
@@ -1,4 +1,5 @@
 using CarRental.Api.Configuration;
+using CarRental.Api.Middleware;
 using CarRental.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.RegisterInfrastructure(builder.Configuration);
 builder.Services.ConfigureSwagger(builder.Configuration);
 
+builder.Services.AddTransient<ExceptionMiddleware>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +27,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.MapControllers();
 
