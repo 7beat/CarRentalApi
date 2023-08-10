a222a4f690ba758a6d3e1a96e4ca0a61bafab5ad
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:40:41 +0200

    Added AuthResult

---
+++ CarRental.Application/Models/Auth/AuthResult.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRental.Application.Models.Auth;
+
+public class AuthResult
+{
+    public string Token { get; set; }
+    public DateTime Expiration { get; set; }
+    public string RefreshToken { get; set; }
+
+    public bool Succeded { get; init; }
+}
