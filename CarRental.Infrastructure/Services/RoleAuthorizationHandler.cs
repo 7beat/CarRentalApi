36cc489f1ec078e936f8aaaf06d3d7d8e8759c05
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:51:06 +0200

    Added Custom RoleAuthorizationHandler

---
+++ CarRental.Infrastructure/Services/RoleAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarRental.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+
+namespace CarRental.Infrastructure.Services;
+public class RoleAuthorizationHandler : AuthorizationHandler<RolesAuthorizationRequirement>
+{
+    private readonly UserManager<ApplicationUser> userManager;
+
+    public RoleAuthorizationHandler(UserManager<ApplicationUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, RolesAuthorizationRequirement requirement)
+    {
+        if (context.User.Identity.IsAuthenticated)
+        {
+            var user = await userManager.FindByNameAsync(context.User.Identity.Name);
+            var roles = await userManager.GetRolesAsync(user);
+
+            if (requirement.AllowedRoles.Intersect(roles).Any())
+            {
+                context.Succeed(requirement);
+                return;
+            }
+        }
+
+        context.Fail();
+    }
+}
