dd28b4010044638f38ce6011e1ff60a86130f2af
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:40:53 +0200

    Added ApplicationRegistration

--- CarRental.Application/ApplicationServicesRegistration.cs
+++ CarRental.Application/ApplicationServicesRegistration.cs
@@ -1,6 +1,14 @@
-﻿namespace CarRental.Application;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
-public class ApplicationServicesRegistration
+namespace CarRental.Application;
+
+public static class ApplicationServicesRegistration
 {
+    public static void RegisterApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
+    }
 }
