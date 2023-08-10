3a3eaddc4fff41c87ef6937cd31030ab82a9de0c
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:41:24 +0200

    Added User

---
+++ CarRental.Infrastructure/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CarRental.Infrastructure.Identity.Models;
+public class ApplicationUser : IdentityUser<int>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    // public DateOnly Birthday { get; set; }
+    // public ICollection<Vehicle> Vehicles { get; set; }
+}
