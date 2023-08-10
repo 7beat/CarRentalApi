e3058ee2136077dea9e014a3497a4da2a454678d
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:40:27 +0200

    Added Auth Commands

---
+++ CarRental.Application/Features/Auth/LoginCommand.cs
@@ -0,0 +1,27 @@
+﻿using CarRental.Application.Contracts.Identity;
+using CarRental.Application.Models.Auth;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Application.Features.Auth.Commands;
+
+public record LoginCommand : IRequest<AuthResult>
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; init; }
+}
+
+public class LoginCommandHandler : IRequestHandler<LoginCommand, AuthResult>
+{
+    private readonly IAuthService authService;
+    public LoginCommandHandler(IAuthService authService)
+    {
+        this.authService = authService;
+    }
+
+    public async Task<AuthResult> Handle(LoginCommand request, CancellationToken cancellationToken) => await authService.LoginAsync(request);
+}
---
+++ CarRental.Application/Features/Auth/RegisterCommand.cs
@@ -0,0 +1,38 @@
+﻿using CarRental.Application.Contracts.Identity;
+using CarRental.Application.Models.Auth;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Application.Features.Auth.Commands;
+public record RegisterCommand : IRequest<AuthResult>
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; }
+    [Required]
+    public string Username { get; init; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; init; }
+    [Required]
+    public string FirstName { get; init; }
+    [Required]
+    public string LastName { get; init; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateOnly Birthday { get; init; }
+}
+
+internal class RegisterCommandHandler : IRequestHandler<RegisterCommand, AuthResult>
+{
+    private readonly IAuthService authService;
+    public RegisterCommandHandler(IAuthService authService)
+    {
+        this.authService = authService;
+    }
+
+    public async Task<AuthResult> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        return await authService.RegisterAsync(request);
+    }
+}
