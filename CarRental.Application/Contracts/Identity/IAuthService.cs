5dd18b9550e736c7dcb0fe5e507398b762e9f357
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-10 23:40:16 +0200

    Added IAuthService Contract

---
+++ CarRental.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using CarRental.Application.Features.Auth.Commands;
+using CarRental.Application.Models.Auth;
+
+namespace CarRental.Application.Contracts.Identity;
+public interface IAuthService
+{
+    Task<AuthResult> LoginAsync(LoginCommand request);
+    Task<AuthResult> RegisterAsync(RegisterCommand request);
+}
