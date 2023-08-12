3861dc1a363277827290a0f45af8df206e5a4b94
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-12 13:07:55 +0200

    Added VehicleDto

---
+++ CarRental.Application/Features/Vehicles/VehicleDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CarRental.Application.Features.Vehicles;
+public record VehicleDto
+{
+    public int Id { get; init; }
+    public string Brand { get; init; }
+    public string Model { get; init; }
+    public DateOnly DateOfProduction { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? NumberOfDoors { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? NumberOfWheels { get; init; }
+    public string VehicleType { get; set; }
+
+    public string Engine { get; init; }
+}
