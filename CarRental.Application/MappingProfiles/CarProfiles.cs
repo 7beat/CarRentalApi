f39d8f6d200e3f5571f37269f9f9837df8f8ba9c
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-13 00:41:58 +0200

    Added Car mapping

---
+++ CarRental.Application/MappingProfiles/CarProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using CarRental.Application.Features.Cars.Commands;
+using CarRental.Domain.Entities;
+
+namespace CarRental.Application.MappingProfiles;
+internal class CarProfiles : Profile
+{
+    public CarProfiles()
+    {
+        CreateMap<AddCarCommand, Car>();
+    }
+}
