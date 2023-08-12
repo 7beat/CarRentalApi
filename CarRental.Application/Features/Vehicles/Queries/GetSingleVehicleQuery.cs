577b14600c519ef0024ef499507bb2ff39cd2b91
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-12 13:22:44 +0200

    Added GetSingleVehicleQuery

---
+++ CarRental.Application/Features/Vehicles/Queries/GetSingleVehicleQuery.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CarRental.Application.Contracts.Persistence;
+using MediatR;
+
+namespace CarRental.Application.Features.Vehicles.Queries;
+public record GetSingleVehicleQuery(int Id) : IRequest<VehicleDto>;
+
+internal class GetSingleVehicleQueryHandler : IRequestHandler<GetSingleVehicleQuery, VehicleDto>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public GetSingleVehicleQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<VehicleDto> Handle(GetSingleVehicleQuery request, CancellationToken cancellationToken)
+    {
+        var vehicle = await _unitOfWork.Vehicle.FindSingleAsync(v => v.Id == request.Id, cancellationToken);
+        return _mapper.Map<VehicleDto>(vehicle);
+    }
+}
