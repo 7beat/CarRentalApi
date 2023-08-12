12753ef882e803ea4c810a198121baaaf7264499
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-12 02:13:12 +0200

    Added Entities

--- CarRental.Infrastructure/Persistence/Data/ApplicationDbContext.cs
+++ CarRental.Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -1,13 +1,37 @@
-﻿using CarRental.Infrastructure.Identity.Models;
+﻿using CarRental.Domain.Common;
+using CarRental.Domain.Entities;
+using CarRental.Infrastructure.Identity.Models;
+using CarRental.Persistence.Data;
+using CarRental.Utility.Converters;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRental.Infrastructure.Persistence.Data;
-internal class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
 
+    }
+
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Engine> Engines { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Motorcycle> Motorcycles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Vehicle>().UseTpcMappingStrategy();
+        builder.ConfigureEntities();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateOnly>()
+            .HaveConversion<DbDateOnlyConverter>()
+            .HaveColumnType("date");
     }
 }
