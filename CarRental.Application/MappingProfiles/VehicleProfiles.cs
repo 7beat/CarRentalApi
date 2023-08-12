4fb4ca5f49ac0092f7f198ad49f6790070702a1c
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-12 13:08:22 +0200

    Added Vehicle Mapping

---
+++ CarRental.Application/MappingProfiles/VehicleProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarRental.Application.Features.Vehicles;
+
+namespace CarRental.Application.MappingProfiles;
+internal class VehicleProfiles : Profile
+{
+    public VehicleProfiles()
+    {
+        CreateMap<Domain.Entities.Car, VehicleDto>()
+            .ForMember(dest => dest.Engine, opt => opt.MapFrom(src => src.Engine.Model));
+
+        CreateMap<Domain.Entities.Motorcycle, VehicleDto>()
+            .ForMember(dest => dest.Engine, opt => opt.MapFrom(src => src.Engine.Model));
+    }
+}
