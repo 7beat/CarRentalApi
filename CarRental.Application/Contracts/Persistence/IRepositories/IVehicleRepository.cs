2331e4fc9398115e07a416fab9058ceaec419ba6
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-12 21:54:04 +0200

    Added FindSingleOfType

--- CarRental.Application/Contracts/Persistence/IRepositories/IVehicleRepository.cs
+++ CarRental.Application/Contracts/Persistence/IRepositories/IVehicleRepository.cs
@@ -4,4 +4,5 @@ namespace CarRental.Application.Contracts.Persistence.IRepositories;
 public interface IVehicleRepository : IGenericRepository<Vehicle>
 {
     Task<IEnumerable<Vehicle>> FindAllOfType(VehicleType vehicleType);
+    Task<Vehicle> FindSingleOfType(int id, VehicleType vehicleType);
 }
