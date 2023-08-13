f30681d60cf99b36d8fba804c5a4ba39a6dc0ae2
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-13 14:02:54 +0200

    Added GetAll

---
+++ CarRental.Api/Controllers/VehiclesController.cs
@@ -0,0 +1,24 @@
+﻿using CarRental.Application.Features.Vehicles.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CarRental.Api.Controllers;
+[ApiController]
+[Route("[controller]")]
+[SwaggerTag("Displaying all available vehicles")]
+public class VehiclesController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public VehiclesController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await mediator.Send(new GetAllVehiclesQuery());
+        return result is null ? NoContent() : Ok(result);
+    }
+}
