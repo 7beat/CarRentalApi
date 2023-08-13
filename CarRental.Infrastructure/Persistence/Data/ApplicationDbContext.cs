42e3488b133d3e6177524b2f536f1be40c0ff5d0
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-13 12:32:50 +0200

    Better Entities configuration

--- CarRental.Infrastructure/Persistence/Data/ApplicationDbContext.cs
+++ CarRental.Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -1,31 +1,28 @@
 ﻿using CarRental.Domain.Common;
 using CarRental.Domain.Entities;
 using CarRental.Infrastructure.Identity.Models;
-using CarRental.Persistence.Data;
 using CarRental.Utility.Converters;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace CarRental.Infrastructure.Persistence.Data;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-
     }
 
-    public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<Engine> Engines { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<Car> Cars { get; set; }
     public DbSet<Motorcycle> Motorcycles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.Entity<Vehicle>().UseTpcMappingStrategy();
-        builder.ConfigureEntities();
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
--- CarRental.Infrastructure/Persistence/Data/EntitiesConfiguration.cs
+++ CarRental.Infrastructure/Persistence/Data/EntitiesConfiguration.cs
@@ -1,70 +1,87 @@
 ﻿using CarRental.Domain.Common;
 using CarRental.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CarRental.Persistence.Data;
-internal static class EntitiesConfiguration
+internal class EnginesConfiguration : IEntityTypeConfiguration<Engine>
 {
-    public static void ConfigureEntities(this ModelBuilder builder)
+    public void Configure(EntityTypeBuilder<Engine> builder)
     {
-        builder.Entity<Vehicle>().HasKey(v => v.Id);
-        builder.Entity<Vehicle>().HasOne(v => v.Engine).WithMany().HasForeignKey(v => v.EngineId);
-
-        builder.Entity<Engine>().HasKey(e => e.Id);
-        builder.Entity<Engine>().HasMany(e => e.Vehicles).WithOne(v => v.Engine).HasForeignKey(v => v.EngineId);
-        builder.Entity<Engine>().Property(e => e.FuelType).HasConversion<string>();
-
-
-        builder.Entity<Engine>().HasData(GetEngines());
-        builder.Entity<Car>().HasData(GetCars());
-        builder.Entity<Motorcycle>().HasData(GetMotorcycles());
-
-    }
-
-    private static IEnumerable<Engine> GetEngines()
-    {
-        return new List<Engine>()
+        builder.HasKey(e => e.Id);
+        builder.HasMany(e => e.Vehicles).WithOne(v => v.Engine).HasForeignKey(v => v.EngineId);
+        builder.Property(e => e.FuelType).HasConversion<string>();
+        builder.HasData(new Engine()
         {
-            new()
-            {
-                Id = 1,
-                Model = "TestEngine",
-                Horsepower = 1,
-                Displacement = 1,
-                FuelType = FuelType.Gasoline,
-            }
-        };
-    }
-
-    private static IEnumerable<Car> GetCars()
-    {
-        return new List<Car>()
+            Id = 1,
+            Model = "1.9 TDI",
+            Displacement = 2000,
+            FuelType = FuelType.Diesel,
+            Horsepower = 150
+        }, new Engine()
         {
-            new()
-            {
-                Id = 1,
-                Brand = "Ford",
-                Model = "Mondeo",
-                DateOfProduction = new(2019, 05, 01),
-                NumberOfDoors = 5,
-                EngineId = 1,
-            }
-        };
-    }
-
-    private static IEnumerable<Motorcycle> GetMotorcycles()
-    {
-        return new List<Motorcycle>()
+            Id = 2,
+            Model = "2.0 Turbo",
+            Displacement = 2200,
+            FuelType = FuelType.Gasoline,
+            Horsepower = 200
+        }, new Engine()
         {
-            new()
-            {
-                Id = 2,
-                Brand = "Kawasaki",
-                Model = "Ninja",
-                DateOfProduction = new(2016, 03, 13),
-                NumberOfWheels = 2,
-                EngineId = 1,
-            }
-        };
+            Id = 3,
+            Model = "KAWASAKI Z1",
+            Displacement = 1000,
+            FuelType = FuelType.Gasoline,
+            Horsepower = 150,
+        });
+    }
+}
+
+internal class VehiclesConfiguration : IEntityTypeConfiguration<Vehicle>
+{
+    public void Configure(EntityTypeBuilder<Vehicle> builder)
+    {
+        builder.UseTpcMappingStrategy();
+        builder.HasKey(v => v.Id);
+        builder.HasOne(v => v.Engine).WithMany(e => e.Vehicles).HasForeignKey(v => v.EngineId);
+    }
+}
+
+internal class CarsConfiguration : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.HasData(new Car()
+        {
+            Id = 1,
+            Brand = "Ford",
+            Model = "Mondeo",
+            DateOfProduction = new(2019, 05, 01),
+            NumberOfDoors = 5,
+            EngineId = 2,
+        }, new()
+        {
+            Id = 2,
+            Brand = "Volkswagen",
+            Model = "Golf",
+            DateOfProduction = new(2007, 04, 4),
+            NumberOfDoors = 3,
+            EngineId = 1
+        });
+    }
+}
+
+internal class MotorcyclesConfiguration : IEntityTypeConfiguration<Motorcycle>
+{
+    public void Configure(EntityTypeBuilder<Motorcycle> builder)
+    {
+        builder.HasData(new Motorcycle()
+        {
+            Id = 3,
+            Brand = "Kawasaki",
+            Model = "Ninja",
+            DateOfProduction = new(2016, 03, 13),
+            NumberOfWheels = 2,
+            EngineId = 3
+        });
     }
 }
