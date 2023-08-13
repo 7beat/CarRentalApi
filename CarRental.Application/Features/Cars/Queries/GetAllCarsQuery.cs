8033565fd1b3741e66153a2fe04035e6f9411a4c
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-13 15:24:38 +0200

    Added GetAllCarsQuery

---
+++ CarRental.Application/Features/Cars/Queries/GetAllCarsQuery.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CarRental.Application.Contracts.Persistence;
+using CarRental.Application.Features.Vehicles;
+using MediatR;
+
+namespace CarRental.Application.Features.Cars.Queries;
+public record GetAllCarsQuery : IRequest<IEnumerable<VehicleDto>>;
+
+internal class GetAllCarsQueryHandler : IRequestHandler<GetAllCarsQuery, IEnumerable<VehicleDto>>
+{
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+    public GetAllCarsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
+
+    public async Task<IEnumerable<VehicleDto>> Handle(GetAllCarsQuery request, CancellationToken cancellationToken)
+    {
+        var cars = await unitOfWork.Car.FindAllAsync(cancellationToken);
+        return mapper.Map<IEnumerable<VehicleDto>>(cars);
+    }
+}
