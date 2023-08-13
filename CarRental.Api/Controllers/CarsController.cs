c89aba6e88d17e66675b811d72d851a8402b8fa5
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-13 15:24:26 +0200

    Added CarsController

---
+++ CarRental.Api/Controllers/CarsController.cs
@@ -0,0 +1,23 @@
+﻿using CarRental.Application.Features.Cars.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class CarsController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public CarsController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await mediator.Send(new GetAllCarsQuery());
+
+        return result is null ? NoContent() : Ok(result);
+    }
+}
