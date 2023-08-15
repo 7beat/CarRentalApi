532ec7455e0562eaf423451b2553102c39f0ae9b
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-15 12:07:57 +0200

    Added Login endpoint

---
+++ CarRental.Api/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using CarRental.Application.Features.Auth.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public AuthController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Login([FromForm] LoginCommand request)
+    {
+        var result = await mediator.Send(request);
+
+        return result.Succeded ? Ok(new { result.Token }) : BadRequest();
+    }
+}
