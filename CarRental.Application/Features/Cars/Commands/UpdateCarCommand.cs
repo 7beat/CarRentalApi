94fb764ef5d4ea9bde7af08cbddc6c411c3df8e9
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-22 19:17:19 +0200

    Added UpdateCommandHandler

---
+++ CarRental.Application/Features/Cars/Commands/UpdateCarCommand.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CarRental.Application.Contracts.Persistence;
+using CarRental.Application.Features.Vehicles;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Application.Features.Cars.Commands;
+public record UpdateCarCommand : IRequest<VehicleDto>
+{
+    [Required]
+    public int Id { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+}
+
+internal class UpdateCarCommandHandler : IRequestHandler<UpdateCarCommand, VehicleDto>
+{
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+    public UpdateCarCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
+
+    public async Task<VehicleDto> Handle(UpdateCarCommand request, CancellationToken cancellationToken)
+    {
+        var car = await unitOfWork.Car.FindSingleAsync(c => c.Id == request.Id, cancellationToken);
+
+        if (car is null)
+            await Console.Out.WriteLineAsync("NotFoundException");
+
+        var carToUpdate = mapper.Map(request, car);
+
+        unitOfWork.Car.Update(carToUpdate!);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return mapper.Map<VehicleDto>(car);
+    }
+}
