63f075ec352a53aa0cf6393aae16fe4319589c7b
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-28 15:17:25 +0200

    Tweaked Query

---
+++ CarRental.Application/Features/Vehicles/Queries/GetAllVehiclesOfTypeQuery.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CarRental.Application.Contracts.Persistence;
+using CarRental.Domain.Common;
+using MediatR;
+
+namespace CarRental.Application.Features.Vehicles.Queries;
+public record GetAllVehiclesOfTypeQuery(VehicleType VehicleType) : IRequest<IEnumerable<VehicleDto>>;
+
+internal class GetSingleVehicleOfTypeQueryHandler : IRequestHandler<GetAllVehiclesOfTypeQuery, IEnumerable<VehicleDto>>
+{
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+    public GetSingleVehicleOfTypeQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
+
+    public async Task<IEnumerable<VehicleDto>> Handle(GetAllVehiclesOfTypeQuery request, CancellationToken cancellationToken)
+    {
+        return mapper.Map<IEnumerable<VehicleDto>>(await unitOfWork.Vehicles.FindAllOfTypeAsync(request.VehicleType, cancellationToken));
+    }
+}
