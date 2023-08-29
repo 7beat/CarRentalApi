4f47186a3196315c0fae30693ed82cd070c4360b
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-29 22:14:06 +0200

    Added Rental

---
+++ CarRental.Domain/Entities/Rental.cs
@@ -0,0 +1,13 @@
+﻿using CarRental.Domain.Common;
+
+namespace CarRental.Domain.Entities;
+public class Rental : EntityBase
+{
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+
+    public Vehicle Vehicle { get; set; }
+    public Guid VehicleId { get; set; }
+
+    public Guid UserId { get; set; }
+}
