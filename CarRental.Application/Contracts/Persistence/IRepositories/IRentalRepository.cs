8ad914afaf247a8701c8867a4fe8666c784f8e50
Tomasz Krzemiński <7beat123@wp.pl>
2023-08-29 22:24:36 +0200

    Added Concept Rental Repository

---
+++ CarRental.Application/Contracts/Persistence/IRepositories/IRentalRepository.cs
@@ -0,0 +1,8 @@
+﻿using CarRental.Application.Features.Rentals;
+using CarRental.Domain.Entities;
+
+namespace CarRental.Application.Contracts.Persistence.IRepositories;
+internal interface IRentalRepository : IGenericRepository<Rental>
+{
+    Task<RentalDto> GetWithUserDetails(Guid id);
+}
