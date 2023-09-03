73f77ca5762af00287992496da3f7dc6920f01b4
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-03 21:58:34 +0200

    Added Post

---
+++ CarRental.Api/Controllers/RentalsController.cs
@@ -0,0 +1,32 @@
+﻿using CarRental.Application.Features.Rentals.Commands;
+using CarRental.Application.Features.Rentals.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Api.Controllers;
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class RentalsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public RentalsController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await mediator.Send(new GetAllRentalsQuery());
+
+        return result.Any() ? Ok(result) : BadRequest();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromForm] AddRentalCommand request)
+    {
+        var result = await mediator.Send(request);
+        return Ok();
+    }
+}
