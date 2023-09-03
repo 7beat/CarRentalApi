c770e57ff84dda6f1882510b53ef0e9921fb59a4
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-03 22:47:26 +0200

    Added Query

---
+++ CarRental.Application/Features/Rentals/Queries/GetSingleRentalQuery.cs
@@ -0,0 +1,20 @@
+﻿using CarRental.Application.Contracts.Persistence.IRepositories;
+using MediatR;
+
+namespace CarRental.Application.Features.Rentals.Queries;
+public record GetSingleRentalQuery(Guid Id) : IRequest<RentalDto>;
+
+internal class GetSingleRentalQueryHandler : IRequestHandler<GetSingleRentalQuery, RentalDto>
+{
+    private readonly IRentalRepository rentalRepository;
+
+    public GetSingleRentalQueryHandler(IRentalRepository rentalRepository)
+    {
+        this.rentalRepository = rentalRepository;
+    }
+
+    public async Task<RentalDto> Handle(GetSingleRentalQuery request, CancellationToken cancellationToken)
+    {
+        return await rentalRepository.GetWithUserDetails(request.Id);
+    }
+}
