5f1cead8dcfdecdb29c0fa646ebedd0e7f8ecca2
Tomasz Krzemiński <7beat123@wp.pl>
2023-09-03 20:50:18 +0200

    Added Rental Mapping

---
+++ CarRental.Application/MappingProfiles/RentalProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CarRental.Application.Features.Rentals;
+using CarRental.Domain.Entities;
+
+namespace CarRental.Application.MappingProfiles;
+internal class RentalProfiles : Profile
+{
+    public RentalProfiles()
+    {
+        CreateMap<Rental, RentalDto>()
+            .ReverseMap();
+    }
+}
