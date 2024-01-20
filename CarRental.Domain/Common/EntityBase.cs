4548562edf1bdb2205016e554ebc7d5f79567ab9
Tomasz Krzemiński <7beat123@wp.pl>
2024-01-20 12:52:40 +0100

    Added CreatedBy and CreatedAt

--- CarRental.Domain/Common/EntityBase.cs
+++ CarRental.Domain/Common/EntityBase.cs
@@ -1,5 +1,7 @@
 ﻿namespace CarRental.Domain.Common;
 public abstract class EntityBase
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; init; }
+    public string? CreatedBy { get; init; }
+    public DateTime CreatedAt { get; init; } = DateTime.Now;
 }
