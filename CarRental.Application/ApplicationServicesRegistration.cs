ce161b5976f450bf2a3d01e65309cf3c03a60d5b
Tomasz Krzemiński <7beat123@wp.pl>
2024-01-24 22:27:29 +0100

    Tweak

--- CarRental.Application/ApplicationServicesRegistration.cs
+++ CarRental.Application/ApplicationServicesRegistration.cs
@@ -25,27 +25,30 @@ public static class ApplicationServicesRegistration
 public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-    private readonly IEnumerable<IValidator<TRequest>> _validators;
+    private readonly IEnumerable<IValidator<TRequest>> validators;
 
     public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
     {
-        _validators = validators;
+        this.validators = validators;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        if (_validators.Any())
-        {
-            var context = new ValidationContext<TRequest>(request);
+        if (!validators.Any())
+            return await next();
 
-            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-            var failures = validationResults.SelectMany(r => r.Errors).Where(f => f is not null).ToList();
+        var context = new ValidationContext<TRequest>(request);
 
-            if (failures.Count != 0)
-            {
-                throw new ValidationException(failures);
-            }
-        }
+        var validationResults = await Task.WhenAll(
+            validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var errors = validationResults
+            .Where(r => r.Errors.Any())
+            .SelectMany(r => r.Errors)
+            .ToList();
+
+        if (errors.Any())
+            throw new ValidationException(errors);
 
         return await next();
     }
