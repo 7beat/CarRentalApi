71bd88484132a17312be027c8477a81233f282cd
Tomasz Krzemiński <7beat123@wp.pl>
2024-02-04 23:59:00 +0100

    Added Rental Message Event

---
+++ CarRental.Application/Contracts/Messaging/Events/RentalCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace CarRental.Infrastructure.Messaging.Events;
+public class RentalCreatedEvent
+{
+    public Guid VehicleId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime DateOfCreation { get; init; } = DateTime.Now;
+}
