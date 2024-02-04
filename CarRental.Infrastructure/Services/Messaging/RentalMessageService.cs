90329c78ea4e3e6fdc8b90f8b9ff4964112ca199
Tomasz Krzemiński <7beat123@wp.pl>
2024-02-05 00:01:13 +0100

    Added Messaging Service

---
+++ CarRental.Infrastructure/Services/Messaging/RentalMessageService.cs
@@ -0,0 +1,18 @@
+﻿using CarRental.Application.Contracts.Messaging.Services;
+using CarRental.Infrastructure.Messaging.Events;
+using MassTransit;
+
+namespace CarRental.Infrastructure.Services.Messaging;
+public class RentalMessageService : IRentalMessageService
+{
+    private readonly IPublishEndpoint publishEndpoint;
+
+    public RentalMessageService(IPublishEndpoint publishEndpoint)
+    {
+        this.publishEndpoint = publishEndpoint;
+    }
+
+    public async Task SendMessageAsync(RentalCreatedEvent message) =>
+        await publishEndpoint.Publish(message);
+
+}
