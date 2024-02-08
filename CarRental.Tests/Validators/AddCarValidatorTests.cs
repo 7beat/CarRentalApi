ccd4ce6b3a4ddf498b5150b65e063fd26fb4f194
Tomasz Krzemiński <7beat123@wp.pl>
2024-02-08 17:39:22 +0100

    Added Validation test

---
+++ CarRental.Tests/Validators/AddCarValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using CarRental.Application.Features.Cars.Commands;
+using FluentValidation.TestHelper;
+
+namespace CarRental.Tests.Validators;
+[TestFixture]
+public class AddCarValidatorTests
+{
+    private AddCarCommandValidator validator;
+
+    [SetUp]
+    public void Setup()
+    {
+        this.validator = new AddCarCommandValidator();
+    }
+
+    [Test]
+    public void GivenValidateAddCarCThenShouldHaveErrorWhenNumberOfDoorsIsTooBig()
+    {
+        var command = new AddCarCommand()
+        {
+            // Brand = "TestBrand",
+            Model = "TestModel",
+            NumberOfDoors = 20,
+            DateOfProduction = new DateOnly(2024, 01, 01),
+            EngineId = Guid.NewGuid(),
+            CreatedBy = Guid.Empty
+        };
+
+        validator.TestValidate(command).ShouldHaveValidationErrorFor(c => c.Brand);
+        validator.TestValidate(command).ShouldHaveValidationErrorFor(c => c.NumberOfDoors);
+    }
+}
