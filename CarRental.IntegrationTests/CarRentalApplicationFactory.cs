a561db137dfc8943a3d7f76b1b6d3df73a9bdaa0
Tomasz Krzemiński <7beat123@wp.pl>
2024-02-15 20:45:23 +0100

    Added AppFactory

---
+++ CarRental.IntegrationTests/CarRentalApplicationFactory.cs
@@ -0,0 +1,77 @@
+﻿using CarRental.Infrastructure.Persistence.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace CarRental.IntegrationTests;
+internal class CarRentalApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+            services.RemoveAll(typeof(AuthenticationOptions));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("CarRentalTestDb");
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "TestScheme";
+                options.DefaultChallengeScheme = "TestScheme";
+            }).AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestScheme", options => { });
+
+            var dbContext = CreateDbContext(services);
+        });
+    }
+
+    private static ApplicationDbContext CreateDbContext(IServiceCollection services)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+        var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.EnsureCreated();
+
+        dbContext.Cars.Add(new()
+        {
+            Id = Guid.Parse("ead2ec73-e5f0-4ab2-be3a-e9c0a043017d"),
+            Brand = "TestCar",
+            Model = "TestModel",
+            DateOfProduction = new(2024, 02, 13),
+            NumberOfDoors = 5,
+            EngineId = Guid.Parse("25a79fdb-b76a-45bf-bc1b-e18487d51212")
+        });
+
+        dbContext.SaveChanges();
+
+        return dbContext;
+    }
+}
+
+public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var identity = new ClaimsIdentity(Array.Empty<Claim>(), "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
