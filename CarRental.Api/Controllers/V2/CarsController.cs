6dc0ff2f2f1296f96d48c0c83c73792cd559c3eb
Tomasz Krzemiński <7beat123@wp.pl>
2024-03-03 22:37:28 +0100

    Added concept V2 CarsController

---
+++ CarRental.Api/Controllers/V2/CarsController.cs
@@ -0,0 +1,28 @@
+﻿using Asp.Versioning;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CarRental.Api.Controllers.V2;
+
+[ApiController]
+[ApiVersion("2")]
+[Route("api/v{apiVersion:apiVersion}/[controller]")]
+[SwaggerTag("Displaying and Managing Cars")]
+public class CarsController : ControllerBase
+{
+    private readonly IMediator mediator;
+    private readonly IMapper mapper;
+    public CarsController(IMediator mediator, IMapper mapper)
+    {
+        this.mediator = mediator;
+        this.mapper = mapper;
+    }
+
+    [HttpGet("[action]")]
+    public IActionResult GetAll()
+    {
+        return Ok("Version 2 GetAll");
+    }
+}
