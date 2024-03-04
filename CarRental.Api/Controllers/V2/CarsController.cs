412c559d63cb003e6f057120f864ae1703fd22ff
Tomasz Krzemiński <7beat123@wp.pl>
2024-03-04 22:41:50 +0100

    Added concept V2

--- CarRental.Api/Controllers/V2/CarsController.cs
+++ CarRental.Api/Controllers/V2/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning;
-using AutoMapper;
+using CarRental.Application.Features.Cars.Queries;
+using CarRental.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -7,22 +8,28 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace CarRental.Api.Controllers.V2;
 
 [ApiController]
-[ApiVersion("2")]
-[Route("api/v{apiVersion:apiVersion}/[controller]")]
+[ApiVersion(2)]
 [SwaggerTag("Displaying and Managing Cars")]
-public class CarsController : ControllerBase
+public class CarsController : BaseApiController
 {
-    private readonly IMediator mediator;
-    private readonly IMapper mapper;
-    public CarsController(IMediator mediator, IMapper mapper)
-    {
-        this.mediator = mediator;
-        this.mapper = mapper;
-    }
+    public CarsController(IMediator mediator) : base(mediator)
+    { }
 
     [HttpGet("[action]")]
-    public IActionResult GetAll()
+    [ProducesResponseType(typeof(IEnumerable<Car>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetAll()
     {
-        return Ok("Version 2 GetAll");
+        var result = await mediator.Send(new GetAllCarsQuery());
+
+        return result is null ? NoContent() : Ok(result);
+    }
+
+    [HttpGet("[action]/{id:guid}")]
+    public async Task<IActionResult> GetById([SwaggerParameter("Id of Car")] Guid id)
+    {
+        var car = await mediator.Send(new GetSingleCarQuery(id));
+
+        return car is null ? NotFound() : Ok(car);
     }
 }
