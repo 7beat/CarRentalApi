cadc342d9f2dc789982166be4b68c20a02cb5ad4
Tomasz Krzemiński <7beat123@wp.pl>
2024-03-21 17:18:42 +0100

    Added Handlers Test

---
+++ CarRental.Tests/Architecture/ApplicationTests.cs
@@ -0,0 +1,24 @@
+﻿using CarRental.Application;
+using MediatR;
+using NetArchTest.Rules;
+
+namespace CarRental.UnitTests.Architecture;
+[TestFixture]
+public class ApplicationTests
+{
+    [Test]
+    public void HandlersShouldHaveProperNameTest()
+    {
+        var result = Types
+            .InAssembly(typeof(ApplicationServicesRegistration).Assembly)
+            .That()
+            .ImplementInterface(typeof(IRequestHandler<,>))
+            .Should()
+            .HaveNameEndingWith("CommandHandler")
+            .Or()
+            .HaveNameEndingWith("QueryHandler")
+            .GetResult();
+
+        Assert.That(result.IsSuccessful, Is.True);
+    }
+}
